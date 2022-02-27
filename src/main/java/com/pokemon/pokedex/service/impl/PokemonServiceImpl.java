49a0438da0d5227b2db9a63b808d59df8cf00f68
GautamBalwant <gautam.balwant@gmail.com>
2022-02-27 22:20:14 +0000

    Added caching using spring in memory cache , for production Redis should be used

--- src/main/java/com/pokemon/pokedex/service/impl/PokemonServiceImpl.java
+++ src/main/java/com/pokemon/pokedex/service/impl/PokemonServiceImpl.java
@@ -3,27 +3,21 @@ package com.pokemon.pokedex.service.impl;
 
 import com.pokemon.pokedex.model.*;
 import com.pokemon.pokedex.service.PokemonService;
-import com.pokemon.pokedex.util.ConfigUtil;
 import com.pokemon.pokedex.util.HelperUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpEntity;
-
 import org.springframework.http.HttpMethod;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 
-
-
 @Service
 public class PokemonServiceImpl implements PokemonService {
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
-    private HelperUtil helperUtil;
+    private final HelperUtil helperUtil;
 
     public PokemonServiceImpl(RestTemplate restTemplate, HelperUtil helperUtil) {
         this.restTemplate = restTemplate;
@@ -31,38 +25,40 @@ public class PokemonServiceImpl implements PokemonService {
     }
 
     @Override
+    @Cacheable(value = "pokemon",key="#name")
     public ResponseDTO getPokemonByName(final String name) {
         return getPokemonDetails(name);
     }
 
     @Override
+    @Cacheable(value = "translation",key="#name")
     public ResponseDTO getTranslatedDescription(final String name) {
-        ResponseDTO response =getPokemonDetails(name);
-        String translatedDescription = getTransalatedDescription(response.isLengendary(),response.getHabitat(),response.getDescription());
+        ResponseDTO response = getPokemonDetails(name);
+        String translatedDescription = getTransalatedDescription(response.isLengendary(), response.getHabitat(), response.getDescription());
         response.setDescription(translatedDescription);
         return response;
     }
 
-    private ResponseDTO getPokemonDetails(final String name){
-        String uri =helperUtil.getPokemonApiUri(name);
-        PokemonAPIResponse res =  restTemplate.getForObject(uri, PokemonAPIResponse.class);
-        PokemonDTO pokemonDTO= res.getSpecies();
-        PokemonSpeciesDTO pokemonSpeciesDTO = restTemplate.getForObject(pokemonDTO.getUrl(),PokemonSpeciesDTO.class);
-        ResponseDTO response = getPokemonResponse(pokemonDTO.getName(),pokemonSpeciesDTO);
-       return response;
+    private ResponseDTO getPokemonDetails(final String name) {
+        String uri = helperUtil.getPokemonApiUri(name);
+        PokemonAPIResponse res = restTemplate.getForObject(uri, PokemonAPIResponse.class);
+        PokemonDTO pokemonDTO = res.getSpecies();
+        PokemonSpeciesDTO pokemonSpeciesDTO = restTemplate.getForObject(pokemonDTO.getUrl(), PokemonSpeciesDTO.class);
+        return getPokemonResponse(pokemonDTO.getName(), pokemonSpeciesDTO);
     }
 
-    private String getTransalatedDescription(final boolean isLengendary, final String habitat, final String description){
-        String uri= helperUtil.getTranslationAPIUri(isLengendary,habitat,description);
+
+    private String getTransalatedDescription(final boolean isLengendary, final String habitat, final String description) {
+        String uri = helperUtil.getTranslationAPIUri(isLengendary, habitat, description);
         HttpEntity httpEntity = helperUtil.getHttpEntity();
         ResponseEntity<TranslationResponse> translated = restTemplate.exchange(uri, HttpMethod.GET, httpEntity, TranslationResponse.class);
-        TranslationResponse translationResponse= translated.getBody();
+        TranslationResponse translationResponse = translated.getBody();
         return translationResponse.getContents().getTranslated();
     }
 
     private ResponseDTO getPokemonResponse(final String name, final PokemonSpeciesDTO pokemonSpeciesDTO) {
         ResponseDTO response = new ResponseDTO();
-        Flavor flavor= pokemonSpeciesDTO.getFlavor_text_entries().get(0);
+        Flavor flavor = pokemonSpeciesDTO.getFlavor_text_entries().get(0);
         response.setName(name);
         response.setHabitat(pokemonSpeciesDTO.getHabitat().getName());
         response.setLengendary(pokemonSpeciesDTO.isIs_legendary());
